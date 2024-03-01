0bd2e541467d072eced10186ac2c80cdb6095fa1
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-03-01 10:59:00 +0100

    add recipe details title

---
+++ Sources/MealzUIModuleIOS/ui/RecipeDetails/MealzRecipeDetailsNumberOfIngredientsTitleView.swift
@@ -0,0 +1,24 @@
+//
+//  MealzRecipeDetailsNumberOfIngredientsTitleView.swift
+//  
+//
+//  Created by Diarmuid McGonagle on 01/03/2024.
+//
+
+import SwiftUI
+import MiamIOSFramework
+
+@available(iOS 14, *)
+public struct MealzRecipeDetailsNumberOfIngredientsTitleView: BaseTitleProtocol {
+    public init() {}
+    public func content(params: TitleParameters) -> some View {
+        return HStack {
+            Text(params.title)
+                .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.titleBigStyle)
+                .foregroundColor(Color.mealzColor(.standardDarkText))
+            Spacer()
+        }
+        .padding(Dimension.sharedInstance.mPadding)
+        .frame(maxWidth: .infinity)
+    }
+}
