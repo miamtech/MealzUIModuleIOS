906f3887d3e0483ff0899b5416f56a5e97f3d499
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-03-28 12:32:50 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         } else {
             dependencies.append(contentsOf: [
                 .package(url: "https://github.com/miamtech/releaseMealz", from: "1.0.0-beta3"),
-                .package(url: "https://github.com/miamtech/MealzIOSFrameworkSPM", exact: "1.0.0-beta3")
+                .package(url: "https://github.com/miamtech/MealzIOSFrameworkSPM", exact: "1.0.0-beta4")
             ]
             )
         }
