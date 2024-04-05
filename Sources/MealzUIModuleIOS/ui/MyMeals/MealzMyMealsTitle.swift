da0a61fd3cd77fed28a3bba8dc03b716801e4d29
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-04-05 10:42:28 +0200

    add my meals title

--- Sources/MealzUIModuleIOS/ui/MyMeals/MealzMyMealsTitle.swift
+++ Sources/MealzUIModuleIOS/ui/MyMeals/MealzMyMealsTitle.swift
@@ -12,13 +12,8 @@ import MealzIOSFramework
 public struct MealzMyMealsTitle: BaseTitleProtocol {
     public init() {}
     public func content(params: TitleParameters) -> some View {
-        EmptyView()
-    }
-}
-
-@available(iOS 14, *)
-struct MealzMyMealsTitle_Previews: PreviewProvider {
-    static var previews: some View {
-        MealzMyMealsTitle().content(params: TitleParameters(title: "Test", subtitle: "Subtitle"))
+        Text(params.title)
+            .miamFontStyle(style: MiamFontStyleProvider.sharedInstance.titleStyle)
+            .foregroundColor(Color.mealzColor(.primaryText))
     }
 }
